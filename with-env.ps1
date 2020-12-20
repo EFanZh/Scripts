c6667e20320cea90b714d2141f1d8518a425366b
EFanZh <efanzh@gmail.com>
2020-12-20 16:13:28 +0800

    Update with-env.ps1

--- with-env.ps1
+++ with-env.ps1
@@ -4,15 +4,15 @@ function With-Env {
     $saved = @{}
 
     foreach ($item in $items.GetEnumerator()) {
-        $saved.Add($item.Key, (Get-Item Env:\$($item.Key) -ErrorAction Ignore).Value)
-        Set-Item -Path Env:\$($item.Key) -Value $item.Value
+        $saved.Add($item.Key, (Get-Item "Env:\$($item.Key)" -ErrorAction Ignore).Value)
+        Set-Item -Path "Env:\$($item.Key)" -Value $item.Value
     }
 
     try {
         & $action
     } finally {
         foreach ($item in $saved.GetEnumerator()) {
-            Set-Item -Path Env:\$($item.Key) -Value $item.Value
+            Set-Item -Path "Env:\$($item.Key)" -Value $item.Value
         }
     }
 }
