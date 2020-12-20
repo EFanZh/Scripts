29e979b79c1565714ac2f06ea43da6c81dbac697
EFanZh <efanzh@gmail.com>
2020-12-20 16:11:01 +0800

    Create with-env.ps1

---
+++ with-env.ps1
@@ -0,0 +1,18 @@
+function With-Env {
+    param ($items, $action)
+
+    $saved = @{}
+
+    foreach ($item in $items.GetEnumerator()) {
+        $saved.Add($item.Key, (Get-Item Env:\$($item.Key) -ErrorAction Ignore).Value)
+        Set-Item -Path Env:\$($item.Key) -Value $item.Value
+    }
+
+    try {
+        & $action
+    } finally {
+        foreach ($item in $saved.GetEnumerator()) {
+            Set-Item -Path Env:\$($item.Key) -Value $item.Value
+        }
+    }
+}
